0648a27fdfc4ac6c029cbba8519e90e215d48a0e
devanand <ndevanand95@gmail.com>
2021-05-29 02:12:26 -0400

    Ingredients show page - End of lec 140

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,4 +1,5 @@
 class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:edit, :update, :show]
   
   def new
     
@@ -17,11 +18,17 @@ class IngredientsController < ApplicationController
   end
   
   def show
-    
+    @ingredient_recipes = @ingredient.recipes.paginate(page: params[:page], per_page: 5)
   end
   
   def index
     @ingredients = Ingredient.paginate(page: params[:page], per_page: 5)
+  end
+  
+  private
+  
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
   end
   
 end
