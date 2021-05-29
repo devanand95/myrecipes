69aca595a0fe6b196773c45ab6f34139724631b8
devanand <ndevanand95@gmail.com>
2021-05-29 13:08:33 -0400

    Added Like/ Dislike feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   root 'pages#home'   
   get 'pages/home', to: 'pages#home'
   
-  resources :recipes do
-    resources :comments, only: [:create]
-  end
+ resources :recipes do
+   resources :comments, only: [:create]
+   member do 
+    post 'like'
+   end
+ end
 
   get '/signup', to: 'chefs#new'
   resources :chefs, except: [:new]
