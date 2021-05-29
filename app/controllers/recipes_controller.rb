f4ce399218d75b37d7d26e129fc5d6973d536b50
devanand <ndevanand95@gmail.com>
2021-05-29 02:59:32 -0400

    Ingredient listing at recipe creation - End of lec 144

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -51,7 +51,7 @@ end
 private
 
 def recipe_params
-  params.require(:recipe).permit(:name, :description)
+  params.require(:recipe).permit(:name, :description, ingredient_ids: [])
 end
 
 def set_recipe
